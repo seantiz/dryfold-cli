ffa4143c1dc480eddd2ae661430c1d4c799c08c9
sean <135666428+seantiz@users.noreply.github.com>
2024-11-08 11:20:15 +0000

    feature(main): Add main() function, walkDirectory() now accepts dynamic arguments.
    refactor(main): Remove superfluous await syntax

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.0.3 (2024-11-04)
+
+Added main() that uses Readline to prompt the user for an entry-point folder when beginning static analysis of the target repo. walkDirectory() now accepts that user input as its argument.
+
 ## 0.0.1 (2024-11-04)
 
 We rewrote our original Javascript file in Typescript and split tasks up into modules. The main responsibilities of this app are to analyse time and complexity, and to generate reports and a .dot graph as well as SVG and PNG graph viz.
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
 import fs from 'fs'
 import path from 'path'
+import { createInterface } from 'readline/promises'
 import { validateBinary } from './utils'
 import { analyseCppDependencies } from './analyse/tasks'
 import { generateGraphs, printFeatureReport, printComplexityReport } from './reports/generate'
 
-const targetDirectory = process.argv[2] || './lib'
+async function main() {
+
+    const cliPrompt = createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    const entryPoint = (await cliPrompt.question('Please type the entry-point folder: ')).trim()
+
+    if (!fs.existsSync(entryPoint)) {
+        console.error(`Couldn't find "${entryPoint}" or it doesn't exist. Please double check, then run the app again.`)
+        cliPrompt.close()
+        return
+    }
+
+    cliPrompt.close()
+
+    const moduleRelationships = walkDirectory(entryPoint)
+            generateGraphs(moduleRelationships)
+            printComplexityReport(moduleRelationships)
+            printFeatureReport(moduleRelationships)
+            cliPrompt.close()
+}
 
 function walkDirectory(dir: string) {
     const moduleMap = new Map()
@@ -33,7 +56,4 @@ function walkDirectory(dir: string) {
     return moduleMap
 }
 
-const moduleRelationships = walkDirectory(targetDirectory)
-generateGraphs(moduleRelationships)
-printComplexityReport(moduleRelationships)
-printFeatureReport(moduleRelationships)
+main().catch(console.error)
